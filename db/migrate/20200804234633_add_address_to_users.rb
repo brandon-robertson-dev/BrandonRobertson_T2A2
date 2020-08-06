403fb30e56e2244b23c8a00f30de9d5ec24c7afc
Brandon <brandonrobertson23@gmail.com>
2020-08-06 13:26:52 +1000

    Adding null to street addresses

--- db/migrate/20200804234633_add_address_to_users.rb
+++ db/migrate/20200804234633_add_address_to_users.rb
@@ -1,5 +1,5 @@
 class AddAddressToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :street_address, :string, default: "Add Address", null: false
+    add_column :users, :street_address, :string, null: false
   end
 end
